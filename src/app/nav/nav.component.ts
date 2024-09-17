1756d7f28d5691151e6acdcd248e83bc61bc1f58
Musa Maluleke <musamalukss@gmail.com>
2024-09-17 12:52:20 +0200

    Implemented logout and injected Accounts services to use the Signal

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,38 +2,33 @@ import { Component, inject, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountsService } from '../_services/accounts.service';
 import { NgIf } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule,NgIf],
+  imports: [FormsModule, NgIf, BsDropdownModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
+  accountServices = inject(AccountsService);
+  model: any = {};
 
-  private accountServices = inject(AccountsService);
-  loggedIn = false;
-  model : any = {};
-
-   login(){
+  login() {
     this.accountServices.login(this.model).subscribe({
-      next: (response: any) =>{ 
-        console.log(response),
-        this.loggedIn=true
+      next: (response) => {
+        console.log(response)
       },
-      error : (error:any) => {
+      error: (error: any) => {
         console.log(error);
       },
-      complete: () => console.log("Request has been completed")
-    
-    })
+      complete: () => console.log('Request has been completed'),
+    });
   }
 
-  logout(){
-    console.log("logout")
-    this.loggedIn=false;
-
+  logout() {
+    console.log('logout');
+    this.accountServices.logout();
   }
-
 }
