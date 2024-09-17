2c8dafba6464a33301d7f68131069f2b9fc97cb6
Musa Maluleke <musamalukss@gmail.com>
2024-09-17 20:43:01 +0200

    Added navigation using routerlinks

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,32 +3,37 @@ import { FormsModule } from '@angular/forms';
 import { AccountsService } from '../_services/accounts.service';
 import { NgIf } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, NgIf, BsDropdownModule],
+  imports: [FormsModule, NgIf, BsDropdownModule,RouterLink,RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
   accountServices = inject(AccountsService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
   model: any = {};
 
   login() {
     this.accountServices.login(this.model).subscribe({
-      next: (response) => {
-        console.log(response)
+      next: () => {
+        this.router.navigateByUrl("/members")
+       
       },
       error: (error: any) => {
-        console.log(error);
-      },
-      complete: () => console.log('Request has been completed'),
+        this.toastr.error(error.error);
+      
+      }
     });
   }
 
   logout() {
-    console.log('logout');
     this.accountServices.logout();
+    this.router.navigateByUrl("");
   }
 }
