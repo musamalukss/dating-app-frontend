bdd2ab1976abc7c82bd3b71125c011f30efe0dab
Musa Maluleke <musamalukss@gmail.com>
2024-09-17 21:21:30 +0200

    Implemented authguards for our routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,20 @@ import { MembersListComponent } from './members/members-list/members-list.compon
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
     {path:'', component: HomeComponent},
-    {path:'members', component: MembersListComponent},
-    {path:'members/:id', component: MemberDetailComponent},
-    {path:'lists', component: ListsComponent},
-    {path:'messages', component: MessagesComponent},
+    {path:'',runGuardsAndResolvers:'always', canActivate:[authGuard],
+        children:[
+            {path:'members', component: MembersListComponent},
+            {path:'members/:id', component: MemberDetailComponent},
+            {path:'lists', component: ListsComponent},
+            {path:'messages', component: MessagesComponent}
+        ]
+
+    },
+
     {path:'**', component: HomeComponent,pathMatch:'full'},
 
 ];
