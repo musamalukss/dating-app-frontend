2be6ab03aa592d829ce00bbb46f752b2ee73d740
Musa Maluleke <musamalukss@gmail.com>
2024-09-23 13:07:40 +0200

    clean up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,37 +1,22 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RegisterComponent } from '../register/register.component';
+import { Member} from '../_models/members';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RegisterComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit {
-
-  ngOnInit(): void {
-    this.getUsers()
-  }
-
-  http = inject(HttpClient)
-  registermode =false;
-  users:any
-  registerToggle(){
-    this.registermode =!this.registermode;
-  } 
-  getUsers(){
-    this.http.get("http://localhost:5001/api/users").subscribe({
-      next: response => this.users = response,
-      error : error => console.error(error),
-      complete: () => console.log("Request has been completed")
-    })
-
+export class HomeComponent {
+  registermode = false;
+  users: any;
+  registerToggle() {
+    this.registermode = !this.registermode;
   }
 
   cancelRegistrationMode(event: boolean) {
     this.registermode = event;
   }
-
 }
