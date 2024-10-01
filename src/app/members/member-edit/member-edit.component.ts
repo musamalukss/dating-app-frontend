b15d9cc647a9ca7487515ce1964a116eed70f425
Musa Maluleke <musamalukss@gmail.com>
2024-10-01 12:25:56 +0200

    Created a member edit component along with Form as a view child and implemented the route guards as well as host listener

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -1,19 +1,30 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, HostListener, inject, OnInit, ViewChild } from '@angular/core';
 import { Member } from '../../_models/members';
 import { AccountsService } from '../../_services/accounts.service';
 import { MemberService } from '../../_services/member.service';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-edit',
   standalone: true,
-  imports: [],
+  imports: [TabsModule, FormsModule],
   templateUrl: './member-edit.component.html',
   styleUrl: './member-edit.component.css',
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm? :NgForm;
+  @HostListener('window;beforeunload',['$event']) hostListener($event:any){
+    if(this.editForm?.dirty){
+      $event.returnvValue=true;
+    }
+  };
   member?: Member;
   private accountService = inject(AccountsService);
   private memberService = inject(MemberService);
+  private tostr =inject(ToastrService);
+
 
   ngOnInit(): void {
     this.loadMember();
@@ -22,14 +33,17 @@ export class MemberEditComponent implements OnInit {
   loadMember() {
     const user = this.accountService.currentUser();
     if (!user) return;
-    this.memberService
-      .getMember(user.username)
-      .subscribe({
-        next: (member) => {
-          console.log(member);
-          this.member = member;
-        },
-      })
-      
+    this.memberService.getMember(user.username).subscribe({
+      next: (member) => {
+        console.log(member);
+        this.member = member;
+      },
+    });
+  }
+
+  UpdateMember() {
+    console.log(this.member);
+    this.tostr.success("Member Updated successfully");
+    this.editForm?.reset(this.member);
   }
 }
