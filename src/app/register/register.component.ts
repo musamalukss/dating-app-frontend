3f6fe1c502203cbd49002711cdae67c30c2c1c87
Musa Maluleke <musamalukss@gmail.com>
2025-01-21 15:50:03 +0200

    Added Validators

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,20 +1,24 @@
 import { Component, inject, input, OnInit, output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AccountsService } from '../_services/accounts.service';
 import { ToastrService } from 'ngx-toastr';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent implements OnInit {
-
-  
   cancelRegister = output<boolean>();
-  registerForm :FormGroup = new FormGroup({})
+  registerForm: FormGroup = new FormGroup({});
   model: any = {};
   private accountService = inject(AccountsService);
   private toastr = inject(ToastrService);
@@ -22,18 +26,23 @@ export class RegisterComponent implements OnInit {
     this.initializeForm();
   }
 
-  initializeForm(){
+  initializeForm() {
     this.registerForm = new FormGroup({
-      username : new FormControl(),
-      password : new FormControl(),
-      confirmPassword : new FormControl(),
-    })
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(10),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(10),
+      ]),
+    });
   }
 
-
-
   register() {
-    console.log(this.registerForm.value)
     // this.accountService.register(this.model).subscribe({
     //   next: () => {
     //     this.toastr.success("Registration Successful!")
